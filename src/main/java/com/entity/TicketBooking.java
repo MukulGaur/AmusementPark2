1f8845589cee9a54fbc8cb84699968a1b5282173
Mukul Gaur <mukulgaur91@gmail.com>
2022-01-03 10:56:55 +0530

    Update TicketBooking.java

--- src/main/java/com/entity/TicketBooking.java
+++ src/main/java/com/entity/TicketBooking.java
@@ -14,6 +14,7 @@ public class TicketBooking {
 //	private Activity activity;
 	private LocalDateTime dateTime;
 	private float bill;
+	private int a;
 	public int getTicketBookingId() {
 		return ticketBookingId;
 	}
